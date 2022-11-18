67a3776b391826c920c4aea7ec495af5a4dfb024
Victor Marin <marinuribe01@gmail.com>
2022-11-18 14:09:39 -0500

    creando-react-app

---
+++ e-comerse/sena-ecomerse/tienda-victor/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Tienda de Victor</a>
+      </header>
+      <main>Listar productos</main>
+    </div>
+  );
+}
+
+export default App;
