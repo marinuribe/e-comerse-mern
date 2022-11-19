76345f1e6cb6f14132ee58b55d8fd37fde2aca32
Victor Marin <marinuribe01@gmail.com>
2022-11-18 23:53:24 -0500

    Agregando-css-fichas

--- e-comerse/sena-ecomerse/tienda-victor/src/App.js
+++ e-comerse/sena-ecomerse/tienda-victor/src/App.js
@@ -1,3 +1,4 @@
+import data from './data';
 
 function App() {
   return (
@@ -5,7 +6,27 @@ function App() {
       <header>
         <a href="/">Tienda de Victor</a>
       </header>
-      <main>Listar productos</main>
+      <main>
+        <h1>Productos destacados</h1>
+        <div className="productos">
+          {data.productos.map((producto) => (
+            <div className="producto" key={producto.ficha}>
+              <a href={`/producto/${producto.ficha}`}>
+                <img src={producto.imagen} alt={producto.nombre} />
+              </a>
+              <div className="producto-info">
+                <a href={`/producto/${producto.ficha}`}>
+                  <p>{producto.nombre}</p>
+                </a>
+                <p>
+                  <strong>${producto.precio} COP</strong>
+                </p>
+                <button>Añadir al carrito</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
