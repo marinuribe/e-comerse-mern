c78093ca83a46f6916eda47df409fd9c724fa704
Victor Marin <marinuribe01@gmail.com>
2022-11-29 12:56:27 -0500

    Creando-metodo-depago

--- e-comerse/sena-ecomerse/tienda-victor/src/Store.js
+++ e-comerse/sena-ecomerse/tienda-victor/src/Store.js
@@ -4,16 +4,19 @@ export const Store = createContext();
 
 const initialState = {
   userInfo: localStorage.getItem('userInfo')
-  ? JSON.parse(localStorage.getItem('userInfo'))
-  : null,
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
 
   cart: {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
     cartItems: localStorage.getItem('cartItems')
-    ? JSON.parse(localStorage.getItem('cartItems'))
-    :[],
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
   },
 };
 
@@ -41,23 +44,29 @@ function reducer(state, action) {
     }
     case 'USER_SIGNIN':
       return { ...state, userInfo: action.payload };
-      case 'USER_SIGNOUT':
-        return {
-          ...state,
-          userInfo: null,
-          cart: {
-            cartItems: [],
-            shippingAddress: {},
-          },
-        };
-      case 'SAVE_SHIPPING_ADDRESS':
-        return {
-          ...state,
-          cart: {
-            ...state.cart,
-            shippingAddress: action.payload,
-          },
-        }; 
+    case 'USER_SIGNOUT':
+      return {
+        ...state,
+        userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: {},
+          paymentMethod: '',
+        },
+      };
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload,
+        },
+      };
+      case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
+      };
     default:
       return state;
   }
