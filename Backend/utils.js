a845b402b69f291fe6d2dabb3beec96dde2c9b13
Victor Marin <marinuribe01@gmail.com>
2022-11-29 18:09:40 -0500

    Creando-tabla-venta

--- Backend/utils.js
+++ Backend/utils.js
@@ -14,3 +14,20 @@ export const generateToken = (user) => {
     }
   ); //encripta los datos
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Token invalido' });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: 'El token no existe' });
+  }
+};
